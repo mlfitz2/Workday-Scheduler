f9407fc3f4477dd20c1682cdd30c8bae1b7c838e
Mike Fitzpatrick <mlfitz2@gmail.com>
2022-01-23 19:26:38 -0800

    activities save to local storage. site is functional

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let currentTimeEl = $('#current-time');
 let scheduleArea = $('#schedule-area')
+let clearPastBtn = $('#clear-past')
 
 //Displays the current time
 function displayTime() {
@@ -16,57 +17,104 @@ let hours = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
 hours.forEach(function(hours) {
 
   //Create rows
-  let calRow = document.createElement('div');
-  calRow.setAttribute('class', 'row');
+  let calRow = $('<div>')
+  $(calRow).attr('class', 'row');
   scheduleArea.append(calRow);
   
   //Create column for the time
-  let hourColumn = document.createElement('div');
-  hourColumn.setAttribute('class', 'col hour-column');
+  let hourColumn = $('<div>');
+  $(hourColumn).addClass('col hour-column')
+  $(hourColumn).attr('id', hours+"-timeslot");
   calRow.append(hourColumn);
-  hourColumn.textContent = moment(hours, 'hh').format('hh A');
+  $(hourColumn).text(moment(hours, 'hh').format('hh A'));
 
       //Change the colors to show which hours are past, present or future
       let currentHour = moment().hours();
       let hourCol = parseInt(hourColumn.textContent);
       if (hours < currentHour) {
-        hourColumn.setAttribute('class', 'past');
+        $(hourColumn).attr('class', 'past');
       } else if (hours > currentHour) {
-        hourColumn.setAttribute('class', 'future')
-      } else if (hours === currentHour) {
-        hourColumn.setAttribute('class', 'present')
+        $(hourColumn).attr('class', 'future');
+      } else {
+        $(hourColumn).attr('class', 'present');
       }
 
-      
   //Create column where the event will go
-  let eventColumn = document.createElement('div');
-  eventColumn.setAttribute('class', 'col events-column');
+  let eventColumn = $('<div>');
+  eventColumn.attr('class', 'col events-column');
   calRow.append(eventColumn);
 
       //Create input field to type in the name of the event to add
-      let eventInput = document.createElement('input');
-      eventInput.setAttribute('type', 'text');
-      eventInput.setAttribute('class', 'form-control event-name')
+      let eventInput = $('<input>');
+      eventInput.attr('type', 'text');
+      eventInput.attr('class', 'form-control event-name');
+      eventInput.attr('id', hours+'-activity');
       eventColumn.append(eventInput);
       
          
       //Create column for Save button
-      let lockColumn = document.createElement('div');
-      lockColumn.setAttribute('class', 'col-2 save-column');
+      let lockColumn = $('<div>');
+      lockColumn.attr('class', 'col-2 save-column');
       calRow.append(lockColumn); 
       
-      let lockIcon = document.createElement('i');
-      lockIcon.setAttribute('class', 'fas fa-lock-open');
+      //Add unlocked icon to Save button
+      let lockIcon = $('<i>');
+      lockIcon.attr('class', 'fas fa-lock-open');
       lockColumn.append(lockIcon); 
       
-      
-      function saveActivity() {
-        lockIcon.removeAttribute('class', 'fas fa-lock-open');
-        lockIcon.setAttribute('class', 'fas fa-lock');
-        lockColumn.setAttribute('class', 'saved')
-      }
-      
-      lockColumn.addEventListener('click', saveActivity);
+      //Clicking the save button saves the event to the calendar on local storage
+      $(lockColumn).click(function() {
+        //When it's saved, the open lock icon is replaced with a closed lock, and the button becomes green
+        lockIcon.removeClass('fas fa-lock-open');
+        lockIcon.addClass('fas fa-lock');
+        lockColumn.addClass('saved');
+
+        //Save activities to local storage  
+        let event0800 = $('#8-activity').val();
+        let event0900 = $('#9-activity').val();
+        let event1000 = $('#10-activity').val();
+        let event1100 = $('#11-activity').val();
+        let event1200 = $('#12-activity').val();
+        let event1300 = $('#13-activity').val();
+        let event1400 = $('#14-activity').val();
+        let event1500 = $('#15-activity').val();
+        let event1600 = $('#16-activity').val();
+        let event1700 = $('#17-activity').val();
+
+        localStorage.setItem('0800-Event', event0800);
+        localStorage.setItem('0900-Event', event0900);
+        localStorage.setItem('1000-Event', event1000);
+        localStorage.setItem('1100-Event', event1100);
+        localStorage.setItem('1200-Event', event1200);
+        localStorage.setItem('1300-Event', event1300);
+        localStorage.setItem('1400-Event', event1400);
+        localStorage.setItem('1500-Event', event1500);
+        localStorage.setItem('1600-Event', event1600);
+        localStorage.setItem('1700-Event', event1700);
+      })
+
+      //Pull activities from local storage when page loads
+      let savedEvent0800 = localStorage.getItem('0800-Event');
+      let savedEvent0900 = localStorage.getItem('0900-Event');
+      let savedEvent1000 = localStorage.getItem('1000-Event');
+      let savedEvent1100 = localStorage.getItem('1100-Event');
+      let savedEvent1200 = localStorage.getItem('1200-Event');
+      let savedEvent1300 = localStorage.getItem('1300-Event');
+      let savedEvent1400 = localStorage.getItem('1400-Event');
+      let savedEvent1500 = localStorage.getItem('1500-Event');
+      let savedEvent1600 = localStorage.getItem('1600-Event');
+      let savedEvent1700 = localStorage.getItem('1700-Event');
+
+      $('#8-activity').val(savedEvent0800);
+      $('#9-activity').val(savedEvent0900);
+      $('#10-activity').val(savedEvent1000);
+      $('#11-activity').val(savedEvent1100);
+      $('#12-activity').val(savedEvent1200);
+      $('#13-activity').val(savedEvent1300);
+      $('#14-activity').val(savedEvent1400);
+      $('#15-activity').val(savedEvent1500);
+      $('#16-activity').val(savedEvent1600);
+      $('#17-activity').val(savedEvent1700);      
     })
     
 
